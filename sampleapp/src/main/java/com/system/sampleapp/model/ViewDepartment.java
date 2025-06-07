d81db106ddf0bcf0efc6b99b60bb08f4ab5b47ed
Udara Nalawansa <udarakavishka13@gmail.com>
2025-06-07 11:57:33 +0530

    feat: Add ViewDepartment model to extend Department with employee count

---
+++ sampleapp/src/main/java/com/system/sampleapp/model/ViewDepartment.java
@@ -0,0 +1,26 @@
+package com.system.sampleapp.model;
+
+import java.sql.Date;
+
+public class ViewDepartment extends Department {
+	private int empcount;
+	
+	public ViewDepartment() {
+		
+	}
+
+	public ViewDepartment(int id,String name, Date established,int employees) {
+		super(id,name,established);
+		this.empcount = employees;
+	}
+
+	public int getEmpcount() {
+		return empcount;
+	}
+
+	public void setEmpcount(int empcount) {
+		this.empcount = empcount;
+	}
+	
+	
+}
